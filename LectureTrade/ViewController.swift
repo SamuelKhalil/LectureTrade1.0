aec8e828ffeccde7ced043b09d6a51c7272533ff
Samuel <sam.khayrallah@yahoo.com>
2018-03-19 22:52:05 -0400

    Initial Commit

---
+++ LectureTrade/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LectureTrade
+//
+//  Created by Samuel Khalil on 3/19/18.
+//  Copyright © 2018 Samuel Khalil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
